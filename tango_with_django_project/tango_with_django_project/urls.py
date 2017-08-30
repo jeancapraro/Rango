7801140b762279bee4f29d2a14ea26d10269e16a
Marco André Mendes <marcoandre@gmail.com>
2017-08-30 11:18:58 -0300

    url vazia também chama o rango

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
                   url(r'^admin/', admin.site.urls),
                   url(r'^rango/', include('rango.urls')),
+                  url(r'^$', include('rango.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
